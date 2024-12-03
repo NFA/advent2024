7d7f6c6fce7ec9e2b140fce4cc70da531e3fbe01
NFA <nfa106@gmail.com>
2024-12-03 15:15:21 +0100

    Utility code + Day 1

---
+++ days/day-01.R
@@ -0,0 +1,23 @@
+library(httr2)
+
+if(!exists("get_aoc_input", mode="function")) source("util.R")
+
+input.file <- get_aoc_input(2024, 1)
+input <- read.table(input.file)
+
+# Part 1
+l1 <- sort(input[,1])
+l2 <- sort(input[,2])
+sum(abs(l1 - l2))
+
+# Part 2
+maxlen <- max(l1, l2)
+l1.tab <- tabulate(l1)
+l1.tab[l1.tab > 0] <-  seq_along(l1.tab)[l1.tab > 0]
+l2.tab <- tabulate(l2)
+
+length(l1.tab) <- maxlen
+length(l2.tab) <- maxlen
+
+sum(l1.tab * l2.tab, na.rm = TRUE)
+
---
+++ days/util.R
@@ -0,0 +1,26 @@
+get_aoc_input <- function(year, day, verbose = FALSE, force_update = FALSE) {
+   local_file = glue::glue("inputs/input-{day}.txt")
+   uri <- glue::glue("https://adventofcode.com/{year}/day/{day}/input", year = year, day = day)
+   sessionCookie <- Sys.getenv("AOC_SESSION")
+   
+   if (file.exists(local_file) && !force_update) {
+      rlang::inform("Input already downloaded, returning its location")
+      return(local_file)
+   }
+   
+   if (sessionCookie == "") {
+      rlang::abort("Session cookie not set in .Renviron file. Please set AOC_SESSION.")
+   }
+   sessionCookie <- glue::glue("session={session_cookie}", session_cookie = sessionCookie)
+   
+   req <- httr2::request(uri) |> httr2::req_headers(Cookie = sessionCookie)
+   
+   rlang::inform("Sending HTTPS request to download input.")
+   resp <- req_perform(req, path = local_file)
+   
+   if (verbose) {
+      print(resp)
+   }
+   
+   return(local_file)
+}
